8cd911487d4920664217c0c6f52f68b212a3cd82
Денис <89923002057@mail.ru>
2021-10-05 19:52:13 +0500

    добавил массив, длинной с лист сделал метод dealCards()

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class Main {
-    private static List<Card> deck = new ArrayList<>();
+    private static List <Card> deck = new ArrayList<>();
+    private static final String [] arrayCard = new String[deck.size()];
     static {
         for (Ranks rank : Ranks.values()) {
             for (Suits suit : Suits.values()) {
@@ -14,9 +12,19 @@ public class Main {
         Collections.shuffle(deck);
         for (Card card : deck) System.out.println(card);
     }
+    private static void dealCards() {
+        while (deck.size() != 0) {
+            System.out.println("нажмите Enter");
+            Scanner scan = new Scanner(System.in);
+            String readString = scan.nextLine();
+            System.out.println("Карта: " + deck.get(arrayCard.length));
+            deck.remove(deck.get(arrayCard.length));
+            if (deck.size() == 0) System.out.println("Колода закончилась");
+        }
+    }
 
 
     public static void main(String[] args) {
-
+        dealCards();
     }
 }
